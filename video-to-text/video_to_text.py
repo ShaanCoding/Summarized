138f2a220239177741dea96dd85b413476f2c9ba
Jarryd <jhar0383@sydney.uni.edu.au>
2021-08-27 23:52:25 +1000

    create async video to text script

---
+++ video-to-text/video_to_text.py
@@ -0,0 +1,28 @@
+gcs_uri = "gs://video_storage_314/yt1s.com - 1 Introduction and Supply  Demand_v144P.mp4"
+
+def transcribe_gcs(gcs_uri):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+    from google.cloud import speech
+
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=2100)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u"Transcript: {}".format(result.alternatives[0].transcript))
+        print("Confidence: {}".format(result.alternatives[0].confidence))
+
+transcribe_gcs(gcs_uri)
