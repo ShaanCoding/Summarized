fb69bbff411fa93fba3c2f2a02751c8798dc1910
cracklarry <71866511+cracklarry@users.noreply.github.com>
2021-08-29 05:06:43 +1000

    Update ankideck.py
    It's done, please change the path to the appropriate path at line 5, and the json file should be formated like:
    ```
    {
      [
        {
          "question": "",
          "answer": ""
        },
        {
          "question": "",
          "answer": ""
        }    
      ]
    }
    ```

--- ankideck.py
+++ ankideck.py
@@ -8,61 +8,68 @@ jsonload = json.load(open(path))
 
 questions = []
 answers = []
-for key in jsonload:
-    key = key.lower()
-    print(key, jsonload[key])
-    if key == "question":
-        questions.append(jsonload[key])
-    else:
-        answers.append(jsonload[key])
+for obj in jsonload:
+    for key in obj:
+        if key == "question":
+            questions.append(obj[key])
+        else:
+            answers.append(obj[key])
 
+# Testing purpose
 # print(questions)
 # print(answers)
 
+qsize = len(questions)
 
-# qsize = len(questions)
-#
-# field_list1 = []
-# template_list = []
-# qnalist = []
-#
-# #Generating categories for the .apkg file in genanki format.
-# for i in range(qsize):
-#     field_list1.append({'name': 'Question{}'})
-#     field_list1.append({'name': 'Answer{}'})
-#     template_list.append({})
-#     template_list[i].update({'name': 'Card {}'.format(i + 1)})
-#     template_list[i].update({'qfmt': '{{Question}}'})
-#     template_list[i].update({'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}'})
-#
-#     qnalist.append(questions[i])
-#     qnalist.append(answers[i])
-#
-# for i in range(2 * qsize):
-#     qnalist[i] = qnalist[i].strip()
-#
+field_list1 = []
+template_list = []
+qnalist = []
+
+# Generating fields and templates under genanki's rules
+for i in range(qsize):
+    field_list1.append({'name': 'Question{}'})
+    field_list1.append({'name': 'Answer{}'})
+    template_list.append({})
+    template_list[i].update({'name': 'Card {}'.format(i + 1)})
+    template_list[i].update({'qfmt': '{{Question}}'})
+    template_list[i].update({'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}'})
+
+    qnalist.append(questions[i])
+    qnalist.append(answers[i])
+
+# Striping spaces and '\n's for unifrom
+for i in range(2 * qsize):
+    qnalist[i] = qnalist[i].strip()
+
+# Printing out the fields, the templates, the questions and the answers.
 # print(field_list1)
 # print(template_list)
 # print(qnalist)
-#
-# my_model = genanki.Model(
-#     1607392319,
-#     'my_model',
-#     fields=field_list1,
-#     templates=template_list
-# )
-#
-# my_note = genanki.Note(
-#     model=my_model,
-#     fields=qnalist)
-#
-# my_deck = genanki.Deck(
-#     2059400110,
-#     'Your Deck')
-#
-# my_deck.add_note(my_note)
-#
-# genanki.Package(my_deck).write_to_file('output.apkg')
+
+# Generating model
+my_model = genanki.Model(
+    1607392319,
+    'my_model',
+    fields=field_list1,
+    templates=template_list
+)
+
+# Generating note
+my_note = genanki.Note(
+    model=my_model,
+    fields=qnalist)
+
+# Generating deck object
+my_deck = genanki.Deck(
+    2059400110,
+    'Your Deck')
+
+# Adding my_note to the deck
+my_deck.add_note(my_note)
+
+# Generating the .apkg file to the current directory.
+genanki.Package(my_deck).write_to_file('output.apkg')
+
 #
 # # Hardcoded
 # my_model = genanki.Model(
