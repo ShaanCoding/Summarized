288d5db5ecd54d01927b3c7f3dd54ddca90a5e6d
Shaan <shaankhan101@gmail.com>
2021-08-28 15:34:56 +1000

    Created Table
    Pushed Table for preview (mockup rest apis)

--- lecture-summarizer/src/Pages/ViewVideo.tsx
+++ lecture-summarizer/src/Pages/ViewVideo.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { Button } from "@material-ui/core";
+import React from "react";
+import Buttons from "../Components/Buttons";
+import Content from "../Components/Content";
+import BasicTable from "../Components/EnhancedTableHead";
+import Footer from "../Components/Footer";
+import InlineFlexbox from "../Components/InlineFlexbox";
+import NavBar from "../Components/NavBar";
+import TitleProp from "../Components/TitleProp";
 
 const viewVideo = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <>
+      <NavBar />
 
-export default viewVideo
+      <Content>
+        <TitleProp
+          title="VIEW VIDEO"
+          description="Our Lecture Summarizer Extracts Videos Into Meaningful and Important Portions Of Information and Summarizes Content For You"
+        />
+
+        <InlineFlexbox>
+          <BasicTable />
+        </InlineFlexbox>
+
+        <InlineFlexbox>
+          <Buttons btnText="Upload Another Video" btnURL="/uploadVideo" />
+        </InlineFlexbox>
+      </Content>
+
+      <Footer />
+    </>
+  );
+};
+
+export default viewVideo;
