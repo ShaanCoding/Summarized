0f467fb9d14837b1c6c9256f8ad9fa590719db22
qtKite <kite@uwu.icu>
2021-08-28 16:51:12 +1000

    migrations

--- backend/summary/views.py
+++ backend/summary/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from rest_framework.views import APIView
+from summary.models import Overview, Summary
+from summary import serializers
 
-# Create your views here.
+class GetOverview(APIView):
+    """
+    View all summaries
+    """
+
+    def get(self, request, format=None):
+        pass
+
+
