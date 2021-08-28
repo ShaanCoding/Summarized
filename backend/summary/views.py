7a6a33a358e14b1c483d59fb16123b36e13de4de
qtKite <kite@uwu.icu>
2021-08-28 22:09:37 +1000

    my code

--- backend/summary/views.py
+++ backend/summary/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.parsers import FileUploadParser
 from summary.models import Overview, Summary
 from summary.serializers import OverviewSerializer, SummarySerializer
 
@@ -23,8 +24,27 @@ class GetOverview(APIView):
 
 class AddSummary(APIView):
     """
-    Add a summary
+    Add a summary using a video.
     """
+    parser_classes = [FileUploadParser]
 
     def post(self, request, format=None):
         pass
+
+
+class UploadFlac(APIView):
+    """
+    Add a summary using a flac.
+    """
+    parser_classes = [FileUploadParser]
+    
+    def post(self, request, format=None):
+        up_file1 = request.FILES['file']
+        up_file2 = request.data['file']
+
+        print(up_file1)
+        print(up_file2)
+        
+        return Response({
+            'status': 'success',
+        }, status=status.HTTP_200_OK)
