3543d0a39a1c4c44ed0870d5a15381a12df36d54
qtKite <kite@uwu.icu>
2021-08-29 04:09:10 +1000

    more refactoring

--- backend/summary/summarize.py
+++ backend/summary/summarize.py
@@ -4,12 +4,11 @@ import requests
 import json
 import sys
 
+ENDPOINT = 'https://sassbook.com/api/ext/summarize/v1'
 API_KEY = "U2FsdGVkX185U9xDNeOQ8xa9O3v2TV+NpWP6KY+nHsAFFvTOMZFqNO/br/UAR/pX6IdZ/sTNHH73Zd7+ZAA/GoLts+Co2qWiCZVVilEKrnP0F/5ktN7BBeLxCPd3wXvFhe+X4V+PW7vI1AozQ5+29hcHTBeFkLk74we8kaCyJV4jB2RMiLIOTUKAtT/D+MHp"
-
 
 def summarize_text(text):
     authHeaderVal = f"Bearer {API_KEY}"
-    endPoint = 'https://sassbook.com/api/ext/summarize/v1'
 
     # Text to summarize (UTF-8)
     text_to_sum = json.load(text)
@@ -29,7 +28,7 @@ def summarize_text(text):
     try:
         # Make the API call
         response = requests.post(
-            endPoint,
+            ENDPOINT,
             data=json.dumps(payload),
             headers=postHeaders,
             timeout=250,
