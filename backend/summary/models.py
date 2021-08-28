eaaf368b78e6f2d0bc1bccf4efc7cd5cb473da09
qtKite <kite@uwu.icu>
2021-08-29 03:55:18 +1000

    removed redundant model

--- backend/summary/models.py
+++ backend/summary/models.py
@@ -4,9 +4,10 @@ class Overview(models.Model):
     summary_id = models.IntegerField(unique=True)
 
 class Summary(models.Model):
+    # file name
     name = models.CharField(max_length=200)
-    duration = models.IntegerField(default=0)
-    video_summary_link = models.CharField(max_length=2100)
+
+    # json field of entire blob
     blob = models.TextField()
     related_tags = models.TextField()
     questions = models.TextField()
