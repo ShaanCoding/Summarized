318d51c559a833c9afcb637092a0d13becf86a00
Jarryd <jhar0383@sydney.uni.edu.au>
2021-08-28 19:42:38 +1000

    keyword extraction preprocessing and feature

---
+++ keyword_extraction/keyword_extractor.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import itertools
+import json
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+def file_to_list(filename):
+    with open(filename) as f:
+        lines = [line.strip() for line in f]
+    return lines
+
+def json_to_list(filename):
+    json_obj = {}
+    with open(filename) as f:
+        json_obj = json.load(f)
+    return json_obj
+        
+
+def bag_of_words(corpus, ngram = (1,1), min_df = 2, max_df = 0.7, stop_words = "english"):
+    vectorizer = TfidfVectorizer(analyzer='word', 
+                             min_df = min_df, 
+                             max_df = max_df,
+                             ngram_range=ngram, 
+                             stop_words=stop_words)
+    fitted = vectorizer.fit_transform(corpus)
+    return vectorizer
+
+def max_sum_sim(doc_embedding, word_embeddings, words, top_n, nr_candidates):
+    # Calculate distances and extract keywords
+    distances = cosine_similarity(doc_embedding, candidate_embeddings)
+    distances_candidates = cosine_similarity(candidate_embeddings, 
+                                            candidate_embeddings)
+
+    # Get top_n words as candidates based on cosine similarity
+    words_idx = list(distances.argsort()[0][-nr_candidates:])
+    words_vals = [candidates[index] for index in words_idx]
+    distances_candidates = distances_candidates[np.ix_(words_idx, words_idx)]
+
+    # Calculate the combination of words that are the least similar to each other
+    min_sim = np.inf
+    candidate = None
+    for combination in itertools.combinations(range(len(words_idx)), top_n):
+        sim = sum([distances_candidates[i][j] for i in combination for j in combination if i != j])
+        if sim < min_sim:
+            candidate = combination
+            min_sim = sim
+
+    return [words_vals[idx] for idx in candidate]
+
+
+# corpus = json_to_list('../video-to-text/long.json')
+# corpus = file_to_list('../video-to-text/long.txt')
+corpus = json_to_list('processed.json')
+ng = 1
+top_n = 10
+nr_candidates = 20
+model = SentenceTransformer('stsb-distilbert-base')
+stop_words = "english"
+try:
+    bow = bag_of_words(corpus, ngram = (ng,ng), max_df = .9, stop_words = stop_words)
+except ValueError:
+    bow = bag_of_words(corpus, ngram = (ng,ng), min_df = 0, max_df = 1, stop_words = stop_words)
+
+doc_embedding = model.encode(corpus, show_progress_bar=True)
+candidates = bow.get_feature_names()
+candidate_embeddings = model.encode(candidates, show_progress_bar=True)
+mss = max_sum_sim(doc_embedding, candidate_embeddings, candidates, top_n, nr_candidates)
+print(mss)
+# print(corpus)
---
+++ keyword_extraction/text_preproc.py
@@ -0,0 +1,93 @@
+import re
+import json
+from nltk.stem import WordNetLemmatizer
+
+def json_to_list(filename):
+    json_obj = {}
+    with open(filename) as f:
+        json_obj = json.load(f)
+    return json_obj
+
+def list_to_json(filename, json_obj):
+    with open(filename, "w") as f:
+        json.dump(json_obj, f)
+
+def decontracted(phrase):
+    """
+        Desc:
+            decontracted takes text and convert contractions into natural form.
+            ref: https://stackoverflow.com/questions/19790188/
+            expanding-english-language-contractions-in-python/47091490#47091490
+            """
+    # specific
+    if type(phrase) != type(""):
+        return phrase
+    phrase = re.sub(r"won\'t", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+    phrase = re.sub(r"won\’t", "will not", phrase)
+    phrase = re.sub(r"can\’t", "can not", phrase)
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    phrase = re.sub(r"n\’t", " not", phrase)
+    phrase = re.sub(r"\’re", " are", phrase)
+    phrase = re.sub(r"\’s", " is", phrase)
+    phrase = re.sub(r"\’d", " would", phrase)
+    phrase = re.sub(r"\’ll", " will", phrase)
+    phrase = re.sub(r"\’t", " not", phrase)
+    phrase = re.sub(r"\’ve", " have", phrase)
+    phrase = re.sub(r"\’m", " am", phrase)
+    return phrase
+
+def lem_text(text_data):
+    """
+        Desc:
+            lem_text takes text and lemmatize it using WordNetLemmatizer.
+            ref: https://stackoverflow.com/a/25535348
+    """
+    if type(text_data) != type(""):
+        return text_data
+    n_text = []
+    for word in text_data.split(" "):
+        n_word = lem.lemmatize(word, pos='a')
+        n_word = lem.lemmatize(n_word, pos='v')
+        n_text.append(n_word)
+    return ' '.join(n_text)
+
+def remove_special_character(phrase, remove_number=False):
+    """
+        Desc:
+            remove_special_character takes text and removes special charcters.
+            ref: https://stackoverflow.com/a/18082370/4084039
+    """
+    if type(phrase) != type(""):
+        return phrase
+    
+    if remove_number:
+        phrase = re.sub('[^A-Za-z]+', ' ', phrase)
+    else:
+        phrase = re.sub('[^A-Za-z0-9]+', ' ', phrase)
+    return phrase
+
+def stop_word_removal(text):
+    stop_words = ['textbook', 'model', 'principles', 'mit', 'teach',
+                    'ebay', 'spend', 'concept', 'raise', 'comes', 'need', 'allocate',
+                    'come', 'want', 'happen', 'se']
+    if type(text) != type(""):
+        return text
+    return ' '.join(e.lower() for e in text.split() if e.lower() not in stop_words)
+
+lem = WordNetLemmatizer()
+json_obj = json_to_list('../video-to-text/long.json')
+json_obj = list(map(decontracted, json_obj)) 
+json_obj = list(map(remove_special_character, json_obj)) 
+json_obj = list(map(lem_text, json_obj)) 
+json_obj = list(map(stop_word_removal, json_obj)) 
+
+list_to_json("processed.json", json_obj)
