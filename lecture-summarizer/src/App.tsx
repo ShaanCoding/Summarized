1f9a7266d4a7764228e9eb269ce0c6d7e81a537d
Shaan <shaankhan101@gmail.com>
2021-08-28 20:51:29 +1000

    Added React Router Parameters
    - Added react router parameters
    - Added tag.tsx
    - Stylized tag.tsx

--- lecture-summarizer/src/App.tsx
+++ lecture-summarizer/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, useParams } from "react-router-dom";
 import index from "./Pages/Index";
 import appMain from "./Pages/AppMain";
 import UploadVideo from "./Pages/UploadVideo";
@@ -8,14 +8,18 @@ import viewVideo from "./Pages/ViewVideo";
 import summarizedVideo from "./Pages/SummarizedVideo";
 
 function App() {
+  const { id } = useParams<{ id: string }>();
+
   return (
-    <Router>
-      <Route exact path="/" component={index} />
-      <Route exact path="/appMain" component={appMain} />
-      <Route exact path="/uploadVideo" component={UploadVideo} />
-      <Route exact path="/viewVideo" component={viewVideo} />
-      <Route exact path="/summarizedVideo" component={summarizedVideo} />
-    </Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={index} />
+        <Route exact path="/appMain" component={appMain} />
+        <Route exact path="/uploadVideo" component={UploadVideo} />
+        <Route exact path="/viewVideo" component={viewVideo} />
+        <Route path={`/summarizedVideo/:id`} component={summarizedVideo} />
+      </Switch>
+    </div>
   );
 }
 
--- lecture-summarizer/src/Pages/SummarizedVideo.tsx
+++ lecture-summarizer/src/Pages/SummarizedVideo.tsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from "react";
+import Buttons from "../Components/Buttons";
+import Content from "../Components/Content";
+import Footer from "../Components/Footer";
+import InlineFlexbox from "../Components/InlineFlexbox";
+import NavBar from "../Components/NavBar";
+import Tag from "../Components/Tag";
+import TitleProp from "../Components/TitleProp";
 
-const summarizedVideo = () => {
-    return (
+const summarizedVideo: React.FC<{ match: any }> = (props) => {
+  console.log(props.match.params.id);
+
+  const handleSubmission = () => {};
+
+  return (
+    <>
+      <NavBar />
+
+      <Content>
+        <TitleProp
+          title="SUMMARIZED LECTURE"
+          description="Our Lecture Summarizer Extracts Videos Into Meaningful and Important Portions Of Information and Summarizes Content For You"
+        />
+
         <div>
-            
+          <h2>Upload Video</h2>
+          <div className="tag-grid">
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+            <Tag tagName="Hello world" />
+          </div>
         </div>
-    )
-}
 
-export default summarizedVideo
+        <div>
+          <h2>Summarized Notes</h2>
+          <div className="summarized-notes">
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi quam et illum asperiores, tenetur saepe cum officia
+                corrupti aspernatur. Consequuntur aperiam eius impedit sapiente.
+                Vero laudantium possimus excepturi laborum sit?
+              </p>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi quam et illum asperiores, tenetur saepe cum officia
+                corrupti aspernatur. Consequuntur aperiam eius impedit sapiente.
+                Vero laudantium possimus excepturi laborum sit?
+              </p>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi quam et illum asperiores, tenetur saepe cum officia
+                corrupti aspernatur. Consequuntur aperiam eius impedit sapiente.
+                Vero laudantium possimus excepturi laborum sit?
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <InlineFlexbox>
+          <Buttons btnText="Download Anki" onClick={handleSubmission} />
+        </InlineFlexbox>
+      </Content>
+
+      <Footer />
+    </>
+  );
+};
+
+export default summarizedVideo;
